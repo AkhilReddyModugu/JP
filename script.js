db9e6878e353be7c9bf6fad0661d1e1f5e9871c6
Akhil <akhilreddymodugu13@gmail.com>
2024-05-16 21:33:30 +0530

    Update index.html script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 async function display() {
-        const response = await axios.get('http://localhost:3000/users');
+        const response = await axios.get('http://localhost:3333/users');
         const users = response.data;
         const tableEle = document.getElementById('res');
         tableEle.innerHTML = '';
 
         users.forEach((user) => {
             const row = document.createElement('tr');
-            row.innerHTML = `
+            row.innerHTML = `   
                 <td>${user.id}</td>
                 <td>${user.name}</td>
                 <td>${user.branch}</td>
+                <td><button onclick="remove(${user.id})">Delete</button></td>
+                <td><button onclick="update(${user.id})">Edit</button></td>
             `;
             tableEle.appendChild(row);
         });
@@ -22,15 +24,27 @@ async function insert() {
             branch: document.getElementById('branch').value
         };
 
-        await axios.post('http://localhost:3000/users', data);
+        await axios.post('http://localhost:3333/users', data);
         alert('New student data inserted!!');
         display();
 }
 
-async function remove(){
-    let uid= prompt('Enter student id to delete');
-    await axios.delete(`http://localhost:3000/users${uid}`);
-    alert(`Student with id: ${uid} deleted`);
+async function remove(id){
+    await axios.delete(`http://localhost:3333/users/${id}`);
+    alert(`Student with id: ${id} deleted`);
     display();
 }
-// window.onload = display;
+
+async function update(id) {
+    let newName = prompt("Enter new name");
+    let newBranch = prompt("Enter new branch");
+
+    const newData = {
+        name: newName,
+        branch: newBranch
+    };
+
+    await axios.put(`http://localhost:3333/users/${id}`, newData);
+    alert(`User with ID ${id} updated successfully`);
+    display();
+}
